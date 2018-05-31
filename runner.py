565d2463febd3348a491d28cb74f44d0850176f5
giannis kwvaios <p13kova@ionio.gr>
2018-05-31 21:46:51 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,193 +1,188 @@
  
 
 import plex
+'''
+Stmt_list-> Stmt Stmt_list | e.
+Stmt->Expr | print Expr.
+Expr-> Term Term_tail.
+Term_tail->Logic Term Term_tail | e.
+Term-> Factor Factor_tail.
+Factor_tail ->Neg Factor Factor_tail | e.
+Factor->(Expr) | id |boolean | e.
+Logic->and|or.
+Neg-> not.
+'''
 
  #dimiourgia klashs parser opou dhlwnontai oi aparaithtes plhrfories kanontas xrhsh toy plex    
+class ParseError(Exception):
+	pass
 
-class MyParser    
-	def _init_(self):
-		self.st = {}
+class MyParser:   
+    def _init_(self):
+        self.st = {}
 
-	def create_scanner(self,fp):
-		letter = plex.Rnage ('azAZ') #euros timwn xaraktira
-		digit = plex.Range ('09') # euros arithmitikwn timwn
+    def create_scanner(self,fp):
+        letter = plex.Range ('azAZ')                  
+        digit = plex.Range ('09')                     
 
 
-		telestis = plex.Str ('=') #dilwsi tou xaraktira '=' sthn metavliti telestis
-		parenthesi = plex.Str ('(',')') #dilwsi tou xaraktira '('')' sthn metavliti parenthesi
-		int_num = plex.Rep1(digit)
-		name = letter+plex.Rep(letter | digit)
+        telestis = plex.Str ('=')                     
+        parenthesi = plex.Str ('(',')')               
+        int_num = plex.Rep1(digit)
+        name = letter+plex.Rep(letter | digit)
+        space = plex.Rep1(plex.Any(' \n\t'))           
+        keyword = plex.Str('print')
+                   
+        logic = plex.NoCase (plex.Str('and','or'))
+        logicNot = plex.NoCase (plex.Str('not'))
+        booleanT = plex.NoCase(plex.Str('true','t','1'))
+        booleanF = plex.NoCase (plex.Str('false','f','0'))
 
-		space = plex.Rep1(plex.Any(' \n\t')) #apofugi kenwn
-		keyword = plex.Str('print') #dilwsi desmeumenis leksis 'print'
-		logic = plex.NoCase (plex.Str('and','not','or')) #dilwsi logikwn metavlitwn
-        boolean = plex.NoCase(plex.Str('true','false','0','1')) #dilwsi boolean metavlitwn
-		
-		lexicon = plex.Lexicon ([  #dhmiourgia toy aparaititoy leksikoy
-			(keyword, plex.Text),
+        lexicon = plex.Lexicon ([                      
+			(keyword, plex.TEXT),
 			(name, 'Identifier'),
 			(space, plex.IGNORE),
 			(telestis, '='),
 			(parenthesi, plex.TEXT),
 			(int_num, 'INTEGER'),
-			(logic, plex.Text),
-			(boolean, plex.Text),
+			(logicNot, 'not'),
+			(booleanT ,'BooleanT'),
+            (booleanF, 'BooleanF'),
+            (logic , plex.TEXT)
 			
 					])
 
-		self.scanner = plex.Scanner(lexicon,fp)
-		self.la,self.val = self.next_token()
+        self.scanner = plex.Scanner(lexicon,fp)
+        self.la,self.val = self.next_token()
 	
-	def parse(self,fp): #klasi upeuthini gia thn dimiourgia tou scanner kai ton orismo ths prwths katastashs ths grammatikis poy dhmioyrghthike
-		self.create.scanner(fp)
-		self.stmt.list()
-		while True:
-			token,text = self.next_token() #orismos toy epomenoy stoixeioy gia anagnwsh
-			if token is None:
-				break
-			print(token,text)
+    def parse(self,fp):
+        self.create_scanner(fp)		
+        self.stmt_list()
+		
+    def match(self,token):		
+        if self.la == token:
+           self.la,self.val = self.next_token()				
+        else:			
+            raise ParseError("Cant match self.la with current token ( self.la: {} current token: {} )".format(self.la,token))
+			
+    def next_token(self):
+        return self.scanner.read()
+    def getValue(self,token,text):
+        pass
 
-
-	def next_token(self): #klasi ypeuthini gia thn eisxwrisi toy epomenoy token ston scanner
-		return self.scanner.read()
-
-	def match(self,token): #klasi ypeuthini gia to matsarisma twn metavlitwn apo to arxeio eisodoy
-		if self.la == token:
-			self.la,self.val = self.next_token()
-		else: 
-			raise ParserError ('waiting token but la came instead')
-
-	def get_value(self,token,text): #klasi pou lamvanei times apo to arxeio eisodou
-		if text in self.st:
-			return self.st[text]
-		else: 
-			raise RunError ('atuxises')
-
-	def stmt_list(self):#prwtos kanonas ths grammatikis poy perimenei san eisodo mia metavliti h thn desmeumeni leksi print kai thn apostelei sto epomeno vima ths grammatikis
-		if self.la == 'Identifier':  
-			self.stmt()
-			self.stmt_list()
+    def stmt_list(self):
+#prwtos kanonas ths grammatikis poy perimenei san eisodo mia metavliti h thn desmeumeni leksi print kai thn apostelei sto epomeno vima ths grammatikis
+        if self.la == 'Identifier':  
+           self.stmt()
+           self.stmt_list()
         elif self.la == 'print':
             self.stmt()
-			self.stmt_list()
-		elif self.la is None:
-			return
-		else:
-			raise ParserError('perimenw identifier h print')
+            self.stmt_list()
+        elif self.la is None:
+           return
+        else:
+            raise ParseError('perimenw identifier h print')
 
-	def stmt(self): #analoga ti exei lavei ws eisodo apostelei tis plirofories sthn klasi expr() h deixnei minima lathous se periptwsh poy den plhroi tis proupotheseis to arxeio eisodou
-		if self.la == 'Identifier' :
-			varname = self.val
-			self.match ('Identifier')
-			self.expr()
+    def stmt(self): #analoga ti exei lavei ws eisodo apostelei tis plirofories sthn klasi expr() h deixnei minima lathous se periptwsh poy den plhroi tis proupotheseis to arxeio eisodou
+        if self.la == 'Identifier' :
+			
+            self.match ('Identifier')
+            self.match ('=')
+            self.expr()
 
-		elif self.la == 'print'
-		     self.match('print')
+        elif self.la == 'print':
+             self.match('print')
              self.expr()
-		else:
-			raise ParserError('perimenw identifier h print')
+        else:
+           raise ParseError('perimenw identifier h print')
 
-	def expr(self): # o sugkekrimenos kanonas ths grammatikis analoga thn eisodo poy lavei apo to proigoymeno vima apostelei tis plirofories. oi apodektes times einai parenthesi, metavliti h kapoia boolean metavliti
+    def expr(self): # o sugkekrimenos kanonas ths grammatikis analoga thn eisodo poy lavei apo to proigoymeno vima apostelei tis plirofories. oi apodektes times einai parenthesi, metavliti h kapoia boolean metavliti
 
-		if self.la == '(' or self.la == 'Identifier' or self.la == 'boolean':
-			t = self.term()
-			tt = self.term_tail()
-			if tt is None:
-				return t
-			if tt[0] == 'or':
-				return t+tt[1], 'or'  #sumfwna me thn grammatiki to prwto poy orizetai einai to 'or' opote anazita ayto
+        if self.la == '(' or self.la == 'Identifier' or self.la == 'booleanT' or  self.la == ('booleanF') or self.la == ('logicNot'):
+            self.term()
+            self.term_tail()
+			
 
-			else:
-				raise ParserError ('perimenontas kati allo')
+        else:
+            raise ParseError ('1perimenontas kati allo')
 #oi epomenes 3 klaseis anagnwrizoyn ton telesth boolean twn dedomenwn kai ekteloun tis analoges energeies symfwna me toys kanones poy exoyn oristei sthn grammatiki
 
     def term_tail(self):   
-        if self.la == 'or':
-            op = self.boolean()
-            t = self.term()
-            tt = self.term_tail()
-            if tt is None:
-               return op,t
-        
-            elif tt[0] == 'or':
-                return op, t, tt, 'or'
-        elif self.la == 'Identifier' or self.la == 'print' or self.la == ')'
-            return 
-
+        if self.la in ('or', 'and'):
+            self.logic()
+            self.term()
+            self.term_tail()
+        elif self.la in ('IDENTIFIER','print',None,')'):
+           return   
         else:
-            raise ParserError ('perimenontas kati allo')
+            raise ParseError ('2perimenontas kati allo')
 
 
     def term(self):
-        if self.la== '(' or self.la == 'boolean' or self.la ==  'Identifier'
-            f = self.neg ()
-            ff = self.factor_tail()
-            if ff is None:
-                return f
-            if ff[0] == 'and':  #anazitisi tou oroy 'and'
-                return f,ff,'and'
-            else:
-                raise ParserError('perimenontas kati allo')
+        if self.la == '(' or self.la == 'booleanT' or self.la == 'booleanF' or self.la == ('logicNot') or self.la == 'Identifier':
+            self.factor ()
+            self.factor_tail()
+            
+        else:
+                raise ParseError('3perimenontas kati allo')
 
     def factor_tail(self):
-        if self.la == 'and':
-            op = self.logic ()
-            f = self.neg()
-            ff = self.factor_tail()
-
-            if ff is None :
-                return op,f
-            elif ff[0] == 'and':
-                return op, f, ff[1], 'and'
+        if self.la in ('not'):
+            self.neg()
+            self.factor()
+            self.factor_tail()
+          
         else:
-            raise ParserError ('peprimenontas kati allo')
+            raise ParseError ('4perimenontas kati allo')
     
-    def neg(self): #klasi h opoia antistrefei tis times twn metavlitwn se periptwsi poy akoloythoyntai apo 'not'
-        neg= False
-        if self.la == 'not':   
-            neg = True
-            self.match ('not')
-            return self.factor(),neg
-        if neg = True and self.la == 'not':
-            neg= False
-            self.match('not')
-            return self.factor(),neg
+
 
     def factor(self):
-        if self.la == '('
+        if self.la == '(':
             self.match('(')
-               e=self.expr()
-               self.match(')')
-               return e
-        elif self.la == 'Identifier'   
-            varname = self.val
-            self.match(self.la)
+            self.expr()
+            self.match(')')
+            return e
+        elif self.la == 'Identifier':   
+             self.match('Identifier')
             
-        elif self.la == "True" or self.la == "False":
-            token = self.la
-            self.match(token)
-            return token   
+        elif self.la == "booleanT":
+            
+            self.match('booleanT')
+        elif self.la == 'booleanF':
+            self.match('booleanF')  
+        elif self.la in ('and','or','not','IDENTIFIER','print',None,')'):
+           return     
         else:
-            raise ParserError ('perimenontas kati allo')
+            raise ParseError ('5perimenontas kati allo')
 
 
-     def logic(self): #klasi pou matsarei tis eiserxomenes logikes times
+    def logic(self): #klasi pou matsarei tis eiserxomenes logikes times
         if self.la == 'and':
             self.match('and')
-            return('and')
+           
         elif self.la == 'or':
             self.match('or')
-            return('or')
-        elif self.la == 'not'
-            self.match ('not')
+           
+        
         else:
             raise ParseError('error')
+
+    def neg(self):
+        if self.la == 'not':
+            self.match ('not')
+        else:
+            raise ParseError ('error')
 
 parser = MyParser()  # adikeimeno ths klasshs myparser
 
 with open('recursive.txt') as fp:
-    try:
-        parser.parse(fp)  # sto adikeimeno ths klasshs myparser na kanei parse
-    except ParseError as perr:
-        print(perr)
-		
+   try:
+
+    print('Checking Syntax \n')
+    parser.parse(fp)
+    print('Syntax is correct !!')
+    
+   except ParseError as perr:
+    print (perr)
+
